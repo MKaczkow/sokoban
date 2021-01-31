f868ee027db26990b7c638f7caa785415401f2d0
Mateusz Brawański <emzi0767@gmail.com>
2021-01-31 15:44:12 +0100

    Major player name input UX overhaul.

--- src/src/main/java/elkaproj/ui/GuiPlayerNameView.java
+++ src/src/main/java/elkaproj/ui/GuiPlayerNameView.java
@@ -3,10 +3,13 @@ package elkaproj.ui;
 import javax.swing.*;
 import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
+import javax.swing.text.DefaultEditorKit;
 import java.awt.*;
+import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.beans.PropertyChangeListener;
 import java.util.HashMap;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -16,11 +19,14 @@ import java.util.stream.Collectors;
  */
 public class GuiPlayerNameView extends JPanel implements KeyListener {
 
-    private static final Set<Character> ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789".chars().mapToObj(x -> (char) x).collect(Collectors.toSet());
+    private static final String ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    private static final Set<Character> ALPHABET_SET = ALPHABET.chars().mapToObj(x -> (char) x).collect(Collectors.toSet());
+    private static final char[] ALPHABET_CHARS = ALPHABET.toCharArray();
 
     private final char[] playerName = "\0\0\0\0\0".toCharArray();
     private final HashMap<String, JTextField> textFields = new HashMap<>();
     private final JButton ok;
+    private final boolean[] dontRefocus = new boolean[]{false, false, false, false, false};
 
     /**
      * Initializes the new player view.
@@ -65,6 +71,9 @@ public class GuiPlayerNameView extends JPanel implements KeyListener {
             text.setForeground(Color.WHITE);
             text.setFont(text.getFont().deriveFont(32f));
             text.setHorizontalAlignment(JTextField.CENTER);
+
+            Action backspaceAction = text.getActionMap().get(DefaultEditorKit.deletePrevCharAction);
+            text.getActionMap().put(DefaultEditorKit.deletePrevCharAction, new BackspaceActionWrapper(text, backspaceAction));
 
             inputPanel.add(text, c);
 
@@ -124,33 +133,81 @@ public class GuiPlayerNameView extends JPanel implements KeyListener {
 
     @Override
     public void keyPressed(KeyEvent keyEvent) {
+        int key = keyEvent.getKeyCode();
+        JTextField textField = (JTextField) keyEvent.getComponent();
+
+        switch (key) {
+            case KeyEvent.VK_ENTER:
+                this.ok.doClick();
+                break;
+
+            case KeyEvent.VK_RIGHT:
+            case KeyEvent.VK_LEFT:
+            case KeyEvent.VK_BACK_SPACE:
+                if (key == KeyEvent.VK_BACK_SPACE && !textField.getText().equals(""))
+                    break;
+
+                int npos = Integer.parseInt(textField.getName());
+                npos += (key == KeyEvent.VK_RIGHT ? 1 : -1);
+
+                if (npos >= 0 && npos <= 4) {
+                    String name = String.valueOf(npos);
+                    JTextField nf = this.textFields.get(name);
+                    nf.grabFocus();
+
+                    if (key == KeyEvent.VK_BACK_SPACE)
+                        nf.setText("");
+                }
+
+                break;
+
+            case KeyEvent.VK_UP:
+            case KeyEvent.VK_DOWN:
+                String sval = textField.getText();
+                if (sval != null && sval.length() > 1)
+                    break;
+
+                char cval = sval != null && sval.length() == 1 ? sval.charAt(0) : '\0';
+                int idx = charIndexOf(cval);
+
+                if (key == KeyEvent.VK_UP)
+                    idx++;
+                else
+                    idx--;
+
+                if (idx >= ALPHABET_CHARS.length)
+                    idx = 0;
+                else if (idx < 0)
+                    idx = ALPHABET_CHARS.length - 1;
+
+                this.dontRefocus[Integer.parseInt(textField.getName())] = true;
+                textField.setText(String.valueOf(ALPHABET_CHARS[idx]));
+                break;
+        }
     }
 
     @Override
     public void keyReleased(KeyEvent keyEvent) {
-        if (keyEvent.getKeyCode() == KeyEvent.VK_ENTER)
-            this.ok.doClick();
+    }
 
-        if (keyEvent.getKeyCode() == KeyEvent.VK_BACK_SPACE) {
-            JTextField textField = (JTextField) keyEvent.getComponent();
-            int pos = Integer.parseInt(textField.getName());
+    private static int charIndexOf(char item) {
+        for (int i = 0; i < GuiPlayerNameView.ALPHABET_CHARS.length; i++)
+            if (GuiPlayerNameView.ALPHABET_CHARS[i] == item)
+                return i;
 
-            if (pos > 0) {
-                String name = String.valueOf(pos - 1);
-                JTextField nf = this.textFields.get(name);
-                nf.grabFocus();
-            }
-        }
+        return -1;
     }
 
     private static class UpdateListener implements DocumentListener {
 
         private final GuiPlayerNameView view;
         private final JTextField textField;
+        private final int position;
 
         public UpdateListener(GuiPlayerNameView view, JTextField textField) {
             this.view = view;
             this.textField = textField;
+            this.position = Integer.parseInt(this.textField.getName());
         }
 
         @Override
@@ -169,17 +226,31 @@ public class GuiPlayerNameView extends JPanel implements KeyListener {
         }
 
         private void processUpdate() {
-            int pos = Integer.parseInt(this.textField.getName());
             String text = this.textField.getText();
 
             if (text == null || text.equals("")) {
-                this.view.updateName(pos, '\0');
+                this.view.updateName(this.position, '\0');
+
+                if (this.position > 0) {
+                    String name = String.valueOf(this.position - 1);
+                    JTextField nf = this.view.textFields.get(name);
+
+                    if (this.textField.hasFocus() && !this.view.dontRefocus[this.position])
+                        nf.grabFocus();
+                }
 
                 return;
             }
 
             boolean pendingUpdate = false;
             if (text.length() > 1) {
+                if (this.position < 4) {
+                    String nextText = text.substring(1, 2);
+                    JTextField nf = this.view.textFields.get(String.valueOf(this.position + 1));
+                    nf.grabFocus();
+                    nf.setText(nextText);
+                }
+
                 text = text.substring(0, 1);
                 pendingUpdate = true;
             }
@@ -191,18 +262,22 @@ public class GuiPlayerNameView extends JPanel implements KeyListener {
                 pendingUpdate = true;
             }
 
-            if (!ALPHABET.contains(first)) {
+            if (!ALPHABET_SET.contains(first)) {
                 text = "";
                 pendingUpdate = true;
             }
 
-            if (pos < 4 && text.length() > 0) {
-                String name = String.valueOf(pos + 1);
+            if (this.position < 4 && text.length() > 0) {
+                String name = String.valueOf(this.position + 1);
                 JTextField nf = this.view.textFields.get(name);
-                nf.grabFocus();
+
+                if (this.textField.hasFocus() && !this.view.dontRefocus[this.position])
+                    nf.grabFocus();
+
+                this.view.dontRefocus[this.position] = false;
             }
 
-            this.view.updateName(pos, text.charAt(0));
+            this.view.updateName(this.position, text.charAt(0));
             if (pendingUpdate)
                 SwingUtilities.invokeLater(new TextUpdater(this.textField, text));
         }
@@ -221,6 +296,53 @@ public class GuiPlayerNameView extends JPanel implements KeyListener {
             public void run() {
                 this.textField.setText(this.text);
             }
+        }
+    }
+
+    private static class BackspaceActionWrapper implements Action {
+
+        private final JTextField textField;
+        private final Action proxiedAction;
+
+        public BackspaceActionWrapper(JTextField textField, Action proxied) {
+            this.textField = textField;
+            this.proxiedAction = proxied;
+        }
+
+        @Override
+        public Object getValue(String s) {
+            return this.proxiedAction.getValue(s);
+        }
+
+        @Override
+        public void putValue(String s, Object o) {
+            this.proxiedAction.putValue(s, o);
+        }
+
+        @Override
+        public void setEnabled(boolean b) {
+            this.proxiedAction.setEnabled(b);
+        }
+
+        @Override
+        public boolean isEnabled() {
+            return this.proxiedAction.isEnabled();
+        }
+
+        @Override
+        public void addPropertyChangeListener(PropertyChangeListener propertyChangeListener) {
+            this.proxiedAction.addPropertyChangeListener(propertyChangeListener);
+        }
+
+        @Override
+        public void removePropertyChangeListener(PropertyChangeListener propertyChangeListener) {
+            this.proxiedAction.removePropertyChangeListener(propertyChangeListener);
+        }
+
+        @Override
+        public void actionPerformed(ActionEvent actionEvent) {
+            if (!this.textField.getText().equals(""))
+                this.proxiedAction.actionPerformed(actionEvent);
         }
     }
 }
